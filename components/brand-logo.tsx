de4aebc88b1f0bf63a7e543f500e9cadd06888a3
maurodepalma <maurodepalma94@gmail.com>
2026-03-24 17:19:51 -0300

    logo fix

--- components/brand-logo.tsx
+++ components/brand-logo.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 import { cn } from "@/lib/utils";
 
 interface BrandLogoProps {
@@ -9,13 +7,12 @@ interface BrandLogoProps {
 export function BrandLogo({ className }: BrandLogoProps) {
   return (
     <span className={cn("flex items-center leading-none", className)}>
-      <Image
-        src="/automata-qa-logo.svg"
+      <img
+        src="automata-qa-logo.svg"
         alt="AutomataQA"
         width={300}
         height={52}
         className="block h-10 w-auto"
-        priority
       />
     </span>
   );
