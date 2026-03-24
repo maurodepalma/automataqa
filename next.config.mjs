548c99e2991f4f6195fbe0c89867d7f9c9e9595d
maurodepalma <maurodepalma94@gmail.com>
2026-03-24 18:07:07 -0300

    fix custom domain asset paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-const repository = process.env.GITHUB_REPOSITORY ?? "";
-const repoName = repository.split("/")[1] ?? "";
-const isUserOrOrgPage = repoName.endsWith(".github.io");
-const basePath = isGithubActions && !isUserOrOrgPage ? `/${repoName}` : "";
-
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   trailingSlash: true,
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
-  ...(basePath
-    ? {
-        basePath,
-        assetPrefix: basePath
-      }
-    : {})
 };
 
 export default nextConfig;
